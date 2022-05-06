76af4520a828c5247d9c912bf9ee90cc58125ab5
Lindsey Iler <lindsey_iler1@baylor.edu>
2022-05-06 17:25:40 -0500

    hate this

--- pizzeria/asgi.py
+++ pizzeria/asgi.py
@@ -1,16 +1,14 @@
 """
-ASGI config for pizzeria project.
-
+ASGI config for Pizzeria project.
 It exposes the ASGI callable as a module-level variable named ``application``.
-
 For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pizzeria.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Pizzeria.settings')
 
 application = get_asgi_application()
