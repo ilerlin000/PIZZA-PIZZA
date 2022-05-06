9fe479343351f921810f9d64a488658444cc4776
Lindsey Iler <lindsey_iler1@baylor.edu>
2022-05-06 14:10:59 -0500

    djangoshell

---
+++ DjangoShell.py
@@ -0,0 +1,23 @@
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pizzeria.settings")
+
+import django
+django.setup()
+
+from pizzas.models import Pizza,Topping
+
+pizzas = Pizza.objects.all()
+
+for p in pizzas:
+    print(p.id)
+    print(p.text)
+    print(p.date_added)
+
+p = Pizza.objects.get(id=2)
+print(p)
+
+toppings = p.topping_set.all()
+
+for t in toppings:
+    print(t)
