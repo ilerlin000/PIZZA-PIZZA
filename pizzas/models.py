e694a747e8878d04573141eaf279b1d7f30f7d11
Lindsey Iler <lindsey_iler1@baylor.edu>
2022-05-05 19:43:33 -0500

    setting up + models

--- pizzas/models.py
+++ pizzas/models.py
@@ -1,3 +1,30 @@
+from enum import _auto_null
 from django.db import models
 
 # Create your models here.
+#NEW MODEL - PIZZA PIZZA
+class Pizza(models.Model):
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
+
+#NEW MODEL - TOPPINGS
+class Topping(models.Model):
+    pizza = models.ForeignKey(Pizza,on_delete=models.CASCADE)
+    name = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name 
+
+#NEW MODEL - COMMENT
+class Comment(models.Model):
+    pizza = models.ForeignKey(Pizza,on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
