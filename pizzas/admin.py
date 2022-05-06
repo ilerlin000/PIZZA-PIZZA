f105e8b7482354b289337997f5d514aaf0faf2ed
Lindsey Iler <lindsey_iler1@baylor.edu>
2022-05-05 20:08:40 -0500

    admin register

--- pizzas/admin.py
+++ pizzas/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Pizza, Topping, Comment
+
+#REGISTER
+admin.site.register(Pizza)
+admin.site.register(Topping)
+admin.site.register(Comment)
