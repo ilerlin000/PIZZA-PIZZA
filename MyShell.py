4de3f1b28abf5decc86541aa4456bbd6bfbb67ee
Lindsey Iler <lindsey_iler1@baylor.edu>
2022-05-06 17:42:09 -0500

    like what

--- MyShell.py
+++ MyShell.py
@@ -1,11 +1,10 @@
-
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pizzeria.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Pizzeria.settings")
 
 import django
 django.setup()
 
-from pizzas.models import Pizza,Topping
+from pizzas.models import Pizza, Topping
 
 pizzas = Pizza.objects.all()
 
